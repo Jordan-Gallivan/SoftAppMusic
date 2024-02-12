7833318145dc1abc5290bae1af0a5276f3d9a0bc
Ryan <Turlingtonryan@gmail.com>
2024-02-12 15:54:01 -0500

    Created '/hello' endpoint

---
+++ BackEnd/src/main/java/com/example/demo/message/Message.java
@@ -0,0 +1,22 @@
+package com.example.demo.message;
+
+public class Message {
+
+    private String message;
+
+    public Message() {
+
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
