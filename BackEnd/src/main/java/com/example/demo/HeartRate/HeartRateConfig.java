cb91273935d8eefbb8c4ff084e12e9ebbdc13072
Ryan <Turlingtonryan@gmail.com>
2024-03-02 19:52:45 -0500

    Created websocket for heart rate monitoring

--- BackEnd/src/main/java/com/example/demo/HeartRate/HeartRateConfig.java
+++ BackEnd/src/main/java/com/example/demo/HeartRate/HeartRateConfig.java
@@ -17,7 +17,8 @@ public class HeartRateConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
     }
 }
--- BackEnd/src/main/java/com/example/demo/HeartRate/HeartRateController.java
+++ BackEnd/src/main/java/com/example/demo/HeartRate/HeartRateController.java
@@ -2,16 +2,29 @@ package com.example.demo.HeartRate;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class HeartRateController {
 
-    @MessageMapping("/heartRate")
-    @SendTo("/topic/musicSuggestion")
-    public String sendHeartRate(HeartRateMessage message) throws Exception {
-        int heartRate = message.getHeartRate();
+    private final SimpMessagingTemplate messagingTemplate;
 
-        return "Current Heart Rate: " + heartRate;
+    public HeartRateController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/heartRate")
+    public void sendHeartRate(HeartRateMessage message, Principal principal) throws Exception {
+        int heartRate = message.getHeartRate();
+        String returnMessage = "Current Heart Rate: " + heartRate;
+
+        messagingTemplate.convertAndSendToUser(
+                principal.getName(),
+                "/queue/musicSuggestion",
+                returnMessage
+        );
     }
 }
